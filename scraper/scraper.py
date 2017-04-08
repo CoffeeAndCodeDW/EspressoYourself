5902f77b0ace2537328e08f7bc9425f9b12beb92
AngelaSchauer <aschauer@iastate.edu>
2017-04-08 10:43:22 -0500

    Doesn't really work

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,15 @@
 import csv
 with open('EspressoYourself.csv', newline='') as csvfile:
-	filereader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-	for row in filereader:
-		print(', '.join(row))
+	filereader = csv.reader(csvfile, delimiter=',', quotechar='|')
+	#for row in filereader:
+		#print(' '.join(row))
+	#for newS in filereader :
+	#	for i in 18 :
+	#		print newS.split(,)[i]
+
+	while 1 == 1 : 
+		newlist = next(filereader)
+	#	for i in 18 :
+	#		print(newlist[i])
+
+		
